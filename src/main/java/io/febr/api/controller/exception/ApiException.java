c7cabad5d956256ce995771d6b77d49dd9bbe9ab
Sergiu Falcusan <sergiu@unibit.tech>
2024-01-15 20:50:26 +0200

    initial commit

--- src/main/java/io/febr/api/controller/exception/ApiException.java
+++ src/main/java/io/febr/api/controller/exception/ApiException.java
@@ -2,18 +2,5 @@ package io.febr.api.controller.exception;
 
 import java.util.List;
 
-public class ApiException {
-    private List<String> errors;
-
-    public ApiException(List<String> errors) {
-        this.errors = errors;
-    }
-
-    public List<String> getErrors() {
-        return errors;
-    }
-
-    public void setErrors(List<String> errors) {
-        this.errors = errors;
-    }
+public record ApiException(List<String> errors) {
 }
