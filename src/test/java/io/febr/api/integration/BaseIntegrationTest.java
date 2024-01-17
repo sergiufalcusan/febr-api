176a3ec6a9bd129da9114693e7c510fa471a163e
Sergiu Falcusan <sergiu@unibit.tech>
2024-01-17 16:26:28 +0200

    Remove redis as it was not used

--- src/test/java/io/febr/api/integration/BaseIntegrationTest.java
+++ src/test/java/io/febr/api/integration/BaseIntegrationTest.java
@@ -4,26 +4,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.lifecycle.Startables;
 import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 @ActiveProfiles("test")
 public abstract class BaseIntegrationTest {
     static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest")).withExposedPorts(5432);
-    static GenericContainer<?> redisContainer = new GenericContainer<>(DockerImageName.parse("redis:latest")).withExposedPorts(6379);
 
     @DynamicPropertySource
     public static void setDatasourceProperties(DynamicPropertyRegistry registry) {
-        Startables.deepStart(postgresContainer, redisContainer).join();
-
         registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
         registry.add("spring.datasource.username", postgresContainer::getUsername);
         registry.add("spring.datasource.password", postgresContainer::getPassword);
-
-        registry.add("spring.redis.host", redisContainer::getHost);
-        registry.add("spring.redis.port", redisContainer::getFirstMappedPort);
     }
 }
